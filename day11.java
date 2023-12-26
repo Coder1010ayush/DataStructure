fabf3fc719b13e00e75628d282d63b4331c00b38
Ayush <coderayush1010@gmail.com>
2023-12-26 19:01:09 +0530

    updated

--- day11.java
+++ day11.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
 class day11 {
 
     // LeetCode 1155. Number of Dice Rolls With Target Sum
@@ -32,6 +35,40 @@ class day11 {
         int ans = helper(n, k, target);
 
         return ans;
+    }
+
+    // 219. Contains Duplicate II
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (nums == null || nums.length < 2 || k == 0)
+            return false;
+        int i = 0;
+        HashSet<Integer> set = new HashSet<Integer>();
+        for (int j = 0; j < nums.length; j++) {
+            if (!set.add(nums[j])) {
+                return true;
+            }
+
+            if (set.size() >= k + 1) {
+                set.remove(nums[i++]);
+            }
+        }
+        return false;
+    }
+
+    // another ways
+    private static boolean alterWays(int[] nums, int k) {
+        if (nums == null || nums.length < 2 || k == 0)
+            return false;
+        HashMap<Integer, Integer> mapp = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int a = nums[i];
+            if (mapp.containsKey(a)) {
+                if (i < mapp.get(a))
+                    return true;
+            }
+            mapp.put(nums[i], i);
+        }
+        return false;
     }
 
     public static void main(String[] args) {
