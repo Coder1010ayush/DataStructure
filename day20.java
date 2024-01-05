2d0a49092d7ebd75fe803e8c834dcb2ab04788bc
Ayush <coderayush1010@gmail.com>
2024-01-05 14:29:00 +0530

    files are added

--- day20.java
+++ day20.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class day20 {
@@ -47,6 +48,56 @@ public class day20 {
 
         if (ans == Integer.MIN_VALUE)
             return 1;
+        return ans;
+    }
+
+    // 203. Remove Linked List Elements
+    private ListNode remove(ListNode root, int val) {
+
+        if (root == null)
+            return null;
+
+        root.next = remove((ListNode) root.next, val);
+
+        if (root.val == val) {
+            return (ListNode) root.next;
+        } else {
+            return root;
+        }
+
+    }
+
+    public ListNode removeElements(ListNode head, int val) {
+        return remove(head, val);
+    }
+
+    // 1019. Next Greater Node In Linked List
+    public int[] nextLargerNodes(ListNode head) {
+        if (head == null)
+            return new int[0];
+        if (head.next == null)
+            return new int[] { 0 };
+
+        ListNode temp = head;
+        ArrayList<Integer> lsi = new ArrayList<Integer>();
+        while (temp != null) {
+            if (temp.next == null)
+                lsi.add(0);
+            else {
+                ListNode rec = (ListNode) temp.next;
+                while (rec != null && rec.val <= temp.val)
+                    rec = (ListNode) rec.next;
+                if (rec == null)
+                    lsi.add(0);
+                else
+                    lsi.add(rec.val);
+            }
+            temp = (ListNode) temp.next;
+        }
+        int[] ans = new int[lsi.size()];
+        for (int i = 0; i < lsi.size(); i++) {
+            ans[i] = lsi.get(i);
+        }
         return ans;
     }
 
